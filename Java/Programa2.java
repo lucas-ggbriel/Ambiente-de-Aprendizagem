73e17ef57df6784c7e6f0db2689e08ec86a1ea63
Lucas <Lucas@D9997NE038.edu.fb.org.br>
2020-11-12 17:17:52 -0300

    Teste

---
+++ Java/Programa2.java
@@ -0,0 +1,11 @@
+
+public class Programa2 {
+
+	public static void main(String[] args) {
+		
+		String mentaq = "As mentalidades utilizadas até o momento foram: Crescimento, atenção ao detalhe e persistência."
+				+ "A mais utilizadam foi,\nsem dúvidas, a de persistência. Uma habilidade também bastante usada foi a paciência, "
+				+ "afinal,\nnão é nada fácil manter o controle em certos momentos.";
+		System.out.println(mentaq);
+	}
+}
