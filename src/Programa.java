f41e23adc223717285bcc293b277577e8acb8ffd
Lucas <Lucas@D9997NE038.edu.fb.org.br>
2020-11-12 16:42:24 -0300

    Enviando arquivos Java

---
+++ src/Programa.java
@@ -0,0 +1,13 @@
+
+public class Programa {
+
+		public static void main(String[] args) {
+			
+			String sobree = "Um dos meus hobbies favoritos é assistir "
+					+ "filmes aos finais de semana, leitura"
+					+ " e também dormir:)\nMinha comida favorita é frango (de todos os jeitos possíveis), e algo que"
+					+ " eu não sou "
+					+ "muito fã é suco Detox. Sou muito legal (mentira),\nbastante amigável (ás vezes) e também solidário.";
+			System.out.println("\n"+sobree);
+		}
+}
