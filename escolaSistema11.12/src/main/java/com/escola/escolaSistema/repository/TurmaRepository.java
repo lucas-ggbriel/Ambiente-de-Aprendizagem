38ea8825763709cfc0a87be937881f99f294c458
Lucas <Lucas@D9997NE038.edu.fb.org.br>
2020-12-11 11:47:41 -0300

    Organizando GIT

---
+++ escolaSistema11.12/src/main/java/com/escola/escolaSistema/repository/TurmaRepository.java
@@ -0,0 +1,9 @@
+package com.escola.escolaSistema.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.escola.escolaSistema.model.Turma;
+
+public interface TurmaRepository extends JpaRepository <Turma, Long>{
+
+}
